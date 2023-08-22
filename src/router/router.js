bb922896e02b1e2cf17db6a107d4b891a9c69b76
amankr1635 <amankr.1635@gmail.com>
2023-08-22 23:07:14 +0530

    backend completed

--- src/router/router.js
+++ src/router/router.js
@@ -15,7 +15,7 @@ router.get('/api/data', async (req, res) => {
     }
   });
 
-  
+
 // This API handles data creation with pre-existing validations. The input in the request body must match existing entries in the database fetched from the provided URL. If a combination of facility and options already exists, a message will be logged indicating that the combination already exists. If the combination is not found, it will print "New combination" along with the corresponding facility_id and option_id.
 
 router.post('/createData', async (req, res) => {
@@ -42,9 +42,10 @@ router.post('/createData', async (req, res) => {
             }
         };
 
-        const findMatchingOption = (data, name) => {
-            for (const facility of data.facilities) {
-                const option = facility.options.find(opt => opt.name === name);
+        const findMatchingOption = (data, facilityName, optionName) => {
+            const facility = data.facilities.find(fac => fac.name === facilityName);
+            if (facility) {
+                const option = facility.options.find(opt => opt.name === optionName);
                 if (option) {
                     return { facility_id: facility.facility_id, options_id: option.id };
                 }
@@ -53,7 +54,7 @@ router.post('/createData', async (req, res) => {
         };
 
         const response = await fetchData();
-        const optionMatch = findMatchingOption(response, body.optionName);
+        const optionMatch = findMatchingOption(response,body.name,body.optionName);
       
         if (optionMatch) {
             console.log("Exclusions :", optionMatch);
@@ -68,7 +69,7 @@ router.post('/createData', async (req, res) => {
             console.log("No match found");
         }
 
-        return res.status(200).send({ status: true, message: "Data processed successfully." });
+        return res.status(200).send({ status: true, message: "Data processed successfully Check Your console." });
 
     } catch (error) {
         console.error("Error occurred:", error);
